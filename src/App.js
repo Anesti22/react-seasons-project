45f23fca423d53b250bd5afdf67cc5d81bc37947
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2020-02-14 10:11:47 +0100

    Update the h1 text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-           <h1> Seasions Project</h1>
+           <h1> React Seasions Project</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
